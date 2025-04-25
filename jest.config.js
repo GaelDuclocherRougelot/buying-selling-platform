1ffa2da4f0ca711f4f0e6a010c52fb0ed935e786
Gael-dr <gaelduclocher.rougelot@quantiq.io>
2025-04-25 16:05:14 +0200

    feat: project initialization

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'next/babel',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+};
